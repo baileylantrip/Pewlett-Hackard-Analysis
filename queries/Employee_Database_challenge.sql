de5be95e33cd356e2ecb9a4b31c2f7a41f378c30
Bailey Lantrip <baileylantrip312@gmail.com>
2021-10-16 18:29:00 -0700

    deliverable 2

--- queries/Employee_Database_challenge.sql
+++ queries/Employee_Database_challenge.sql
@@ -1,3 +1,4 @@
+--Deliverable 1
 -- Create Retirement Titles
 SELECT e.emp_no,
 e.first_name,
@@ -21,9 +22,27 @@ INTO unique_titles
 FROM retirement_titles as rt
 ORDER BY emp_no ASC, to_date DESC;
 
---Create Retiring Titles
+-- Create Retiring Titles
 SELECT COUNT(ut.emp_no),ut.title
 INTO retiring_titles
 FROM unique_titles as ut
 GROUP BY title
 ORDER BY COUNT(title) DESC;
+
+-- Create Mentorship ELigibility 
+SELECT DISTINCT ON (e.emp_no)e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+de.from_date,
+de.to_date,
+t.title
+INTO mentorship_eligibility
+FROM employees as e 
+INNER JOIN dept_emp as de
+ON (e.emp_no=de.emp_no)
+INNER JOIN titles as t 
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
+ORDER BY emp_no;
